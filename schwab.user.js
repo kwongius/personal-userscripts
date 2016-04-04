bc571386114ccf0fba6ebb3fb778fc49ebff4685
Kevin Wong <snarpel@gmail.com>
2016-04-04 17:09:07 -0600

    Fix matching summary page

--- schwab.user.js
+++ schwab.user.js
@@ -5,11 +5,9 @@
 // @version      0.1.0
 // @description  Hide brokerage section on the Account summary page.
 // @author       Kevin Wong
-// @match        https://client.schwab.com/Accounts/Summary/Summary.aspx
+// @match        https://client.schwab.com/Accounts/Summary/Summary.aspx*
 // @grant        GM_addStyle
 // ==/UserScript==
-/* jshint -W097 */
-'use strict';
 
 // TODO: Ensure that the ID always matches the brokerage section
 GM_addStyle('#ctl00_wpm_ac_ac_pba { display: none; }');
